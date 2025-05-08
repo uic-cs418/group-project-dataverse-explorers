5714d282d223131f92be1bb2deb9cc1d5ada3959
Habibah Sohail <habisohail@gmail.com>
2025-05-08 04:21:31 -0500

    Updated habibah.py file with functions

--- habibah.py
+++ habibah.py
@@ -1,10 +1,65 @@
-bias_race_boro = merged_df[['PERP_RACE_y', 'BORO']].dropna()
-race_boro_counts = bias_race_boro.groupby(['PERP_RACE_y', 'BORO']).size().reset_index(name='Arrest Count')
-plt.figure(figsize=(10, 4))
-sns.barplot(data=race_boro_counts, x='PERP_RACE_y', y='Arrest Count', hue='BORO', palette='Set1')
-plt.title('Are racially-biased arrests higher in certain NYC Boroughs?' )
-plt.xlabel('Race')
-plt.ylabel('Number of Arrests')
-plt.xticks(fontsize=6)
-plt.tight_layout()
-plt.show()
+import pandas as pd
+import numpy as np
+import re                        
+from datetime import datetime
+import zipfile
+import matplotlib.pyplot as plt   
+import seaborn as sns
+import plotly.express as px
+
+with zipfile.ZipFile("NYPD_Hate_Crimes.zip", "r") as z:
+    with z.open("NYPD_Hate_Crimes.csv") as f:
+        df_hate_crimes = pd.read_csv(f)
+columns_to_drop = ['Complaint Precinct Code', 'Law Code Category Description',  'PD Code Description','Bias Motive Description','Month Number','Patrol Borough Name','Full Complaint ID']
+df_hate_crimes = df_hate_crimes.drop(columns=[col for col in columns_to_drop if col in  df_hate_crimes.columns])
+df_hate_crimes = df_hate_crimes.drop_duplicates()
+df_hate_crimes = df_hate_crimes.dropna()
+#print(df_hate_crimes.head())
+
+with zipfile.ZipFile("NYPD_Arrest_Data__Year_to_Date_.zip", "r") as z:
+    with z.open("NYPD_Arrest_Data__Year_to_Date_.csv") as f:
+        df_arrest = pd.read_csv(f)
+df_arrest.replace("(null)", np.nan, inplace=True)
+#print("Columns in the Arrest DataFrame:")
+#print(df_arrest.columns)
+columns_to_drop = ['PD_CD', 'PD_DESC', 'KY_CD', 'LAW_CODE', 'LAW_CAT_CD','JURISDICTION_CODE', 'X_COORD_CD', 'Y_COORD_CD','Latitude', 'Longitude', 'New Georeferenced Column'
+]
+columns_to_drop = [col for col in columns_to_drop if col in df_arrest.columns]
+#print(f"Columns to drop: {columns_to_drop}")
+df_arrest.drop(columns=columns_to_drop, inplace=True)
+df_arrest.dropna(inplace=True)
+#print("Cleaned Arrest DataFrame:")
+#print(df_arrest.head())
+
+
+df_shooting = pd.read_csv("NYPD_shooting_incident_data__Historic__.csv")
+df_shooting.replace("(null)", np.nan, inplace=True)
+columns_to_keep = ['OCCUR_DATE', 'BORO', 'VIC_RACE', 'VIC_AGE_GROUP', 'VIC_SEX', 'PERP_SEX', 'PERP_RACE', 'PRECINCT']
+df_shooting = df_shooting[columns_to_keep]
+df_shooting.dropna(inplace=True)
+#print(df_shooting.head())
+
+
+df_hate_crimes.rename(columns={'Arrest Date': 'DATE', 'County': 'BORO'}, inplace=True)
+df_hate_crimes['DATE'] = pd.to_datetime(df_hate_crimes['DATE'])
+df_arrest.rename(columns={'ARREST_DATE': 'DATE', 'ARREST_BORO': 'BORO'}, inplace=True)
+df_arrest['DATE'] = pd.to_datetime(df_arrest['DATE'])
+df_shooting.rename(columns={'OCCUR_DATE': 'DATE'}, inplace=True)
+df_shooting['DATE'] = pd.to_datetime(df_shooting['DATE'])
+merged_df = pd.merge(df_arrest, df_shooting, on=['DATE', 'BORO'], how='outer')
+merged_df = pd.merge(merged_df, df_hate_crimes, on=['DATE', 'BORO'], how='outer')
+#print(merged_df.head())
+merged_df.to_csv("merged_nypd_data.csv", index=False)
+#print(merged_df.head(2))
+
+def Visualization():
+    bias_race_boro = merged_df[['PERP_RACE_y', 'BORO']].dropna()
+    race_boro_counts = bias_race_boro.groupby(['PERP_RACE_y', 'BORO']).size().reset_index(name='Arrest Count')
+    plt.figure(figsize=(10, 4))
+    sns.barplot(data=race_boro_counts, x='PERP_RACE_y', y='Arrest Count', hue='BORO', palette='Set1')
+    plt.title('Are racially-biased arrests higher in certain NYC Boroughs?' )
+    plt.xlabel('Race')
+    plt.ylabel('Number of Arrests')
+    plt.xticks(fontsize=6)
+    plt.tight_layout()
+    plt.show()
